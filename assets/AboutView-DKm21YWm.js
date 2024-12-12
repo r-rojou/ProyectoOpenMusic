f696d8a6abc5d385eace4b18212276c4d31c8ccf
Raúl <rrojo.u@gmail.com>
2024-12-12 20:54:47 -0300

    Primer commit proyecto

---
+++ assets/AboutView-DKm21YWm.js
@@ -0,0 +1,1 @@
+import{_ as o,c as s,a as t,o as a}from"./index-iWEyy2k_.js";const c={},n={class:"about"};function r(_,e){return a(),s("div",n,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const i=o(c,[["render",r],["__scopeId","data-v-08cd7554"]]);export{i as default};
